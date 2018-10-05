072eba7015fd02b56bf3f744cf3e7ddca02908f3
Sathish <tsatiz@gmail.com>
2018-10-05 17:25:12 +0530

    Build fix - try7

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# Manually build OpenSSL. The openssl create requires 1.0.2+, but Travis CI
+# Manually build OpenSSL. The openssl crate requires 1.0.2+, but Travis CI
 # only includes 1.0.0.
 wget https://www.openssl.org/source/openssl-1.1.0h.tar.gz
 tar xzf openssl-1.1.0h.tar.gz
@@ -14,6 +14,17 @@ rustup target add armv7-unknown-linux-gnueabihf
 
 cargo test
 cargo build --release --target x86_64-unknown-linux-gnu
-OPENSSL_DIR=../openssl-1.1.0h PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target armv7-unknown-linux-gnueabihf
+# cd /tmp
+
+# wget https://www.openssl.org/source/openssl-1.0.1t.tar.gz
+# tar xzf openssl-1.0.1t.tar.gz
+# export MACHINE=armv7
+# export ARCH=arm
+# export CC=arm-linux-gnueabihf-gcc
+# cd openssl-1.0.1t && ./config shared && make && cd -
+
+export OPENSSL_LIB_DIR=../openssl-1.1.0h
+export OPENSSL_INCLUDE_DIR=../openssl-1.1.0h/include
+PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target armv7-unknown-linux-gnueabihf
 cargo deb --no-build --variant=x86_64 
 cargo deb --no-build --variant=armv7
