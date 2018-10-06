d7a30420612bd0cae5aaf31c7884429950123e2e
Sathish <tsatiz@gmail.com>
2018-10-06 09:15:55 +0530

    Build fix - try11

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ cargo build --release --target x86_64-unknown-linux-gnu
 # export CC=arm-linux-gnueabihf-gcc
 # cd openssl-1.0.1t && ./config shared && make && cd -
 
-export OPENSSL_LIB_DIR=./openssl-1.1.0h
+export OPENSSL_DIR=./openssl-1.1.0h
 export OPENSSL_INCLUDE_DIR=./openssl-1.1.0h/include
 
 PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target armv7-unknown-linux-gnueabihf
