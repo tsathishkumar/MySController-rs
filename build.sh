f2218954ab2ba41dc45e5c4979187af5d8da535d
Sathish <tsatiz@gmail.com>
2018-10-08 12:13:15 +0530

    Build fix - try22

--- build.sh
+++ build.sh
@@ -22,8 +22,6 @@ cargo build --release --target x86_64-unknown-linux-gnu
 # export CC=arm-linux-gnueabihf-gcc
 # cd openssl-1.0.1t && ./config shared && make && cd -
 sudo dpkg --add-architecture armhf
-
-deb [arch=armel,armhf] http://ports.ubuntu.com/ubuntu-ports trusty-updates main universe
 sudo apt-get update
 
 sudo apt-get install libudev-dev:armhf
