dc692f5798304026ebd26b80a9381c18187a4d70
Sathish <tsatiz@gmail.com>
2018-10-17 17:41:52 +0530

    Install libudev-dev as part of the build

--- build.sh
+++ build.sh
@@ -12,7 +12,8 @@ cd ..
 sudo apt-get install -qq gcc-arm-linux-gnueabihf
 rustup target add armv7-unknown-linux-gnueabihf
 
-cargo install cross
+apt-get install libudev-dev
+
 cross test
 cross build --target armv7-unknown-linux-gnueabihf
 cargo deb --target x86_64-unknown-linux-gnu
