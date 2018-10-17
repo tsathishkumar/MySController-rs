b06283deec8ec3159446fdad21959c0c06824fcb
Sathish <tsatiz@gmail.com>
2018-10-17 17:17:58 +0530

    Added cross to support armv7 compilation

--- build.sh
+++ build.sh
@@ -10,26 +10,10 @@ sudo make install >/dev/null
 sudo ldconfig
 cd ..
 sudo apt-get install -qq gcc-arm-linux-gnueabihf
-# rustup target add armv7-unknown-linux-gnueabihf
+rustup target add armv7-unknown-linux-gnueabihf
 
-cargo test
-# cargo build --release --target x86_64-unknown-linux-gnu
-
-# wget https://www.openssl.org/source/openssl-1.0.1t.tar.gz
-# tar xzf openssl-1.0.1t.tar.gz
-# export MACHINE=armv7
-# export ARCH=arm
-# export CC=arm-linux-gnueabihf-gcc
-# cd openssl-1.0.1t && ./config shared && make && cd -
-# sudo dpkg --add-architecture armhf
-# sudo apt-get update
-
-# sudo apt-get install libudev-dev:armhf
-
-# export OPENSSL_LIB_DIR=/home/travis/build/tsathishkumar/MySController-rs/openssl-1.1.1
-# export OPENSSL_INCLUDE_DIR=/home/travis/build/tsathishkumar/MySController-rs/openssl-1.1.1/include
-
-# PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target armv7-unknown-linux-gnueabihf
-cargo deb --target x86_64-unknown-linux-gnu 
-#--variant=x86_64
+cargo install cross
+cross test
+cross build --target armv7-unknown-linux-gnueabihf
+cargo deb --target x86_64-unknown-linux-gnu
 # cargo deb --no-build --variant=armv7
