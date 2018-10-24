2c707f896b2bfc7b1bfa043ec3c863c1137fe0dc
Sathish <tsatiz@gmail.com>
2018-10-24 18:22:49 +0530

    Build fix - try 4

--- build.sh
+++ build.sh
@@ -1,28 +1,15 @@
 #!/bin/bash -e
 # Manually build OpenSSL. The openssl crate requires 1.0.2+, but Travis CI
 # only includes 1.0.0.
-wget https://www.openssl.org/source/openssl-1.1.1.tar.gz
-tar xzf openssl-1.1.1.tar.gz
-cd openssl-1.1.1
-./config --prefix=/usr/local shared
-make >/dev/null
-sudo make install >/dev/null
-sudo ldconfig
-cd ..
-sudo apt-get update
-sudo apt-get install -qq gcc-arm-linux-gnueabihf
-rustup target add armv7-unknown-linux-gnueabihf
-
-sudo apt-get install libmagickwand-dev
-sudo apt-get install libudev-dev
-
-SYSROOT=/build/root
-
-export PKG_CONFIG_ALLOW_CROSS=1
-export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
-
-sudo find / -type f -name "libudev.pc"
-PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig cross test
-PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig cross build --target armv7-unknown-linux-gnueabihf
+# wget https://www.openssl.org/source/openssl-1.1.1.tar.gz
+# tar xzf openssl-1.1.1.tar.gz
+# cd openssl-1.1.1
+# ./config --prefix=/usr/local shared
+# make >/dev/null
+# sudo make install >/dev/null
+# sudo ldconfig
+# cd ..
+cross test --target armv7-unknown-linux-gnueabihf
+cross build --target armv7-unknown-linux-gnueabihf
 cargo deb --target x86_64-unknown-linux-gnu
 # cargo deb --no-build --variant=armv7
