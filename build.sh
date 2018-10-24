d3f85fb2c832358fc4ecef2849daadaaf26e7385
Sathish <tsatiz@gmail.com>
2018-10-24 17:35:57 +0530

    Setting pkgconfig path

--- build.sh
+++ build.sh
@@ -18,10 +18,8 @@ sudo apt-get install libudev-dev
 
 SYSROOT=/build/root
 
-export PKG_CONFIG_DIR=
-export PKG_CONFIG_LIBDIR=${SYSROOT}/usr/lib/pkgconfig:${SYSROOT}/usr/share/pkgconfig
-export PKG_CONFIG_SYSROOT_DIR=${SYSROOT}
 export PKG_CONFIG_ALLOW_CROSS=1
+export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
 
 sudo find / -type f -name "libudev.pc"
 cross test
