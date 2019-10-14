3f941ab57ca177e2deb772e46086fc3129d25fd8
Sathish <tsatiz@gmail.com>
2019-10-14 11:20:32 +0530

    Fix docker repo name in push command

--- docker_push.sh
+++ docker_push.sh
@@ -2,4 +2,4 @@
 version=$(cat Cargo.toml| grep "^version" | awk -v x=3 '{print $x}' | sed -e 's/^"//' -e 's/"$//')
 docker build -t tsatiz/myscontroller-rs:${version} .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push USER/REPO
+docker push tsatiz/myscontroller-rs:${version}
